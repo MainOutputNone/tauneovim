00677003d93e7956d4bc85fe0cb630070a413141
MainOutputNone <MainOutputNone@proton.me>
2022-06-26 02:30:14 -0300

    Formatação do "desinstalar.sh"

--- desinstalar.sh
+++ desinstalar.sh
@@ -2,11 +2,11 @@
 DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR"
 clear
-printf "\e[1;33m|\e[0m \n⚙️  \e[1mDesinstalando configurações do tau.nvim\n"
+printf "\n\e[1;33m|\e[0m ⚙️  \e[1mDesinstalando configurações do tau.nvim\n"
 rm -rf $HOME/.config/nvim
 rm -rf $HOME/.local/share/nvim
 
-printf "\e[1;32m|\e[0m \n⚙️  \e[1mRemovidos os seguintes diretorios:\n"
-printf "\e[0m- $HOME/.config/nvim"
-printf "\e[0m- $HOME/.local/share/nvim"
-printf "\e[1;31m|\e[0m \n⚙️  \e[1mtau.nvim removido.\n"
+printf "\e[1;32m|\e[0m ⚙️  \e[1mRemovidos os seguintes diretorios:\n"
+printf "\n\e[0m- $HOME/.config/nvim\n"
+printf "\e[0m- $HOME/.local/share/nvim\n\n"
+printf "\e[1;31m|\e[0m ⚙️  \e[1mtau.nvim removido.\n"
